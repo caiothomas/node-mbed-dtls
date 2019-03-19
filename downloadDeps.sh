d87176f5cfc170f1ca2c1aabd2b5df645e1543c8
caiothomas <caio.thomas@hotmail.com>
2019-03-19 20:12:03 -0300

    version

--- downloadDeps.sh
+++ downloadDeps.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 rm mbedtls -rf
-git clone https://github.com/ARMmbed/mbedtls.git mbedtls-2.16
+git clone https://github.com/ARMmbed/mbedtls.git -b mbedtls-2.3
 
-mkdir mbedtls
-cp mbedtls-2.16 mbedtls
-rm -rf mbedtls-2.16
-
+#mkdir mbedtls
+cp -R mbedtls-2.3 mbedtls
+rm -rf mbedtls-2.3
